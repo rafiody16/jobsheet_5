3cb60e01282da886f805b41c8de711a590c6f0f5
rafiody16 <rafiody16@gmail.com>
2023-09-28 12:48:12 +0700

    Jobsheet 5

---
+++ PemilihanPercobaan122.java
@@ -0,0 +1,30 @@
+/**
+ * PemilihanPercobaan122
+ */
+
+import java.util.Scanner;
+
+public class PemilihanPercobaan122 {
+
+    public static void main (String [] args)
+    {
+        Scanner input22 =  new Scanner(System.in);
+
+
+        System.out.println("Masukkan Angka: ");
+        int angka = input22.nextInt();
+
+        String hasil = (angka %2 == 0) ? "Bilangan Genap" : "Bilangan Ganjil";
+        System.out.println("Angka "+ angka +" adalah "+ hasil);
+
+        // if(angka %2 == 0)
+        // {
+        //     System.out.println("Angka "+ angka +" Bilangan Genap");
+        // }
+        // else
+        // {
+        //     System.out.println("Angka "+ angka +" Bilangan Ganjil");
+        // }
+    }
+    
+}
